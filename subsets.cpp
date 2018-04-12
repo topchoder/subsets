6cec3c716450d0a5dfe33ea95a935fb82b86b826
topchoder <anuragmauryag@gmail.com>
2018-04-13 00:21:47 +0530

    C++ code for generating all subsets

---
+++ subsets.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+//printing the subset
+void print(int *c,int c_size)
+{
+	for(int i=0;i<c_size;i++)
+		cout<<c[i]<<" ";
+	cout<<"\n";
+}
+//generating all subsets for a particular subsequence
+void perm(int *b,int b_size,int *c,int c_size,int *v)
+{
+	if(c_size==b_size)
+	{
+		print(c,c_size);
+		return;
+	}
+	for(int i=0;i<b_size;i++)
+	{
+		if(v[i]==0)
+		{
+			c[c_size]=b[i];
+			v[i]=1;
+			perm(b,b_size,c,c_size+1,v);
+			v[i]=0;//backtracking 2
+		}
+	}
+
+}
+//generating a subsequence
+void subsets(int *a,int *b,int idx,int b_size,int n)
+{
+	int *c=new int[b_size];
+	int *v=new int[b_size];
+	memset(v,sizeof(v),0);
+	perm(b,b_size,c,0,v);
+	delete[] c,v;
+	for(int i=idx;i<n;i++)
+	{
+		b[b_size]=a[i];
+		subsets(a,b,i+1,b_size+1,n);//automatic backtracking 1
+	}
+}
+
+signed main()
+{
+	int *a,n,*b;
+	cout<<"n?"<<"\n";
+	cin>>n;
+	a=new int[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	b=new int[n];
+	subsets(a,b,0,0,n);
+	return 0;
+}
